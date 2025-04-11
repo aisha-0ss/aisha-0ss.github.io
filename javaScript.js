82dc96cf5fa0bd31c24cf6ca201f4d78f53c255a
aisha-0ss <aisha.azycz@gmail.com>
2025-04-11 18:29:35 +0400

    Delete javaScript.js

--- javaScript.js
+++
@@ -1,4 +0,0 @@
-// Subtle flicker scanline effect
-document.body.style.backgroundSize = "100% 2px";
-document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.02) 1px, transparent 1px)";
-document.body.style.backgroundRepeat = "repeat-y";
