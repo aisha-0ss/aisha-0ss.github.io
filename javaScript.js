4c1138b771be04b1ff505bae06d53d686167d25d
aisha-0ss <aisha.azycz@gmail.com>
2025-04-11 18:15:51 +0400

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,4 @@
+// Subtle flicker scanline effect
+document.body.style.backgroundSize = "100% 2px";
+document.body.style.backgroundImage = "linear-gradient(rgba(255, 255, 255, 0.02) 1px, transparent 1px)";
+document.body.style.backgroundRepeat = "repeat-y";
